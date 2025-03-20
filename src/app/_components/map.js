b514fb531a9f77e33ac092d4105924fc6060d8f0
Muhammad Hussain <7phussain@gmail.com>
2025-03-20 13:52:24 +0500

    CHANGES ADDED

--- src/app/_components/map.js
+++ src/app/_components/map.js
@@ -1,6 +1,34 @@
+// "use client";
+
+// import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
+// const containerStyle = {
+//   width: "80%",
+//   height: "400px",
+// };
+
+// const GoogleMaps = ({ lat, lon }) => {
+//   // Ensure lat and lon are numbers
+//   const latitude = Number(lat);
+//   const longitude = Number(lon);
+
+//   if (isNaN(latitude) || isNaN(longitude)) {
+//     return <p>Error: Invalid latitude or longitude</p>;
+//   }
+
+//   const center = { lat: latitude, lng: longitude };
+
+//   return (
+//     <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+//       <Marker position={center} />
+//     </GoogleMap>
+//   );
+// };
+
+// export default GoogleMaps;
 "use client";
 
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "80%",
@@ -8,6 +36,12 @@ const containerStyle = {
 };
 
 const GoogleMaps = ({ lat, lon }) => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) return <p>Loading map...</p>;
+
   // Ensure lat and lon are numbers
   const latitude = Number(lat);
   const longitude = Number(lon);
