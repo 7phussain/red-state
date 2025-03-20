d289a2d8fdec3b8109be72ceb16cdbfba492f4d7
Muhammad Hussain <7phussain@gmail.com>
2025-03-20 16:46:43 +0500

    changesd

--- src/app/developers/_components/developersGrid.js
+++ src/app/developers/_components/developersGrid.js
@@ -5,26 +5,28 @@ const DevelopersGrid = ({ developers }) => {
   const router = useRouter();
   return (
     <div className="md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 flex no-scrollbar overflow-x-auto ">
-      {developers.map((item, ind) => {
-        return (
-          <div
-            key={ind}
-            className="min-w-72 flex flex-col gap-3 items-center p-4 cursor-pointer"
-            onClick={() => {
-              router.push(`/developers/${item?.id}`);
-            }}
-          >
-            <img
-              src={`${process.env.NEXT_PUBLIC_BASE_URL.slice(0, -3)}${
-                item?.logo
-              }`}
-              onError={(e) => {
-                e.target.onerror = null; // Prevent infinite loop
-                e.target.src = "/no_image.png"; // Set fallback image
+      {developers
+        ?.filter((item) => item?.logo)
+        .map((item, ind) => {
+          return (
+            <div
+              key={ind}
+              className="min-w-72 flex flex-col gap-3 items-center p-4 cursor-pointer"
+              onClick={() => {
+                router.push(`/developers/${item?.id}`);
               }}
-              className="w-[200px] h-[200px] object-contain"
-            />
-            {/* <div className="w-[200px]">
+            >
+              <img
+                src={`${process.env.NEXT_PUBLIC_BASE_URL.slice(0, -3)}${
+                  item?.logo
+                }`}
+                onError={(e) => {
+                  e.target.onerror = null; // Prevent infinite loop
+                  e.target.src = "/no_image.png"; // Set fallback image
+                }}
+                className="w-[200px] h-[200px] object-contain"
+              />
+              {/* <div className="w-[200px]">
               <Image
                 src="/developer_logo.png"
                 alt="Sea site"
@@ -35,13 +37,13 @@ const DevelopersGrid = ({ developers }) => {
               />
             </div> */}
 
-            <span className="text-primary">{item?.developerName}</span>
-            <button className="border-primary border text-primary text-white py-1 rounded-full px-4">
-              View Properties
-            </button>
-          </div>
-        );
-      })}
+              <span className="text-primary">{item?.developerName}</span>
+              <button className="border-primary border text-primary text-white py-1 rounded-full px-4">
+                View Properties
+              </button>
+            </div>
+          );
+        })}
     </div>
   );
 };
