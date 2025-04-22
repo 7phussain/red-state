97bba3796d51906ec404c26507893919fe5a985f
jimuskanin <workofmuskan@gmail.com>
2025-04-22 13:09:27 +0400

    FIX

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,85 +40,13 @@ export default function RootLayout({ children }) {
         />
         {/* PIXEL */}
         {/* <AnalyticsHead /> */}
-
-        {/* TIKTOK */}
-        <Script
-                id="tiktok-pixel"
-                strategy="afterInteractive"
-                // onLoad={() => console.log("TikTok Pixel loaded successfully")}
-                // onError={(e) => console.error("TikTok Pixel failed to load:", e)}
-            >
-                {`
-            !function (w, d, t) {
-              w.TiktokAnalyticsObject = t;
-              var ttq=w[t]=w[t]||[];
-              ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],
-              ttq.setAndDefer=function(t,e){
-                t[e] = function () { 
-                  t.push([e].concat(Array.prototype.slice.call(arguments, 0))) 
-                }
-              };
-              for(var i=0;i<ttq.methods.length;i++)
-                ttq.setAndDefer(ttq,ttq.methods[i]);
-                ttq.instance=function(t){
-              for( var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)
-                ttq.setAndDefer(e,ttq.methods[n]);
-                return e
-              },ttq.load=function(e,n){
-                var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;
-                ttq._i=ttq._i||{ },
-                ttq._i[e]=[],
-                ttq._i[e]._u=r,
-                ttq._t=ttq._t||{ },
-                ttq._t[e]=+new Date,
-                ttq._o=ttq._o||{ },
-                ttq._o[e]=n||{ };
-                n=document.createElement("script");
-                n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;
-                e=document.getElementsByTagName("script")[0];
-                e.parentNode.insertBefore(n,e)
-              };
-              ttq.load('CPEA9IBC77U1S6M0C760');
-              ttq.page();
-            }(window, document, 'ttq');
-          `}
-            </Script>
-
-            {/* META PIXEL */}
-            <Script
-                id="meta-pixel"
-                strategy="afterInteractive"
-                // onLoad={() => console.log("Meta Pixel loaded successfully")}
-                // onError={(e) => console.error("Meta Pixel failed to load:", e)}
-            >
-                {`
-                    !function(f,b,e,v,n,t,s) {
-                        if(f.fbq)return;
-                        n=f.fbq=function(){
-                            n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                        };
-                        if(!f._fbq)f._fbq=n;
-                        n.push=n;
-                        n.loaded=!0;
-                        n.version='2.0';
-                        n.queue=[];
-                        t=b.createElement(e);
-                        t.async=!0;
-                        t.src=v;
-                        s=b.getElementsByTagName(e)[0];
-                        s.parentNode.insertBefore(t,s)
-                    }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
-                    fbq('init', '1032827804874469');
-                    fbq('track', 'PageView');
-                `}
-            </Script>
       </head>
 
       <body
         className={` ${plusSans.variable} antialiased max-w-[100vw] text-white min-h-screen`}
       >
         {/* PIXELS */}
-        <AnalyticsBody />
+        {/* <AnalyticsBody /> */}
         <ToastContainer position="top-right" autoClose={3000} />
         {/* <Navbar /> */}
         <SubLayout>{children}</SubLayout>
